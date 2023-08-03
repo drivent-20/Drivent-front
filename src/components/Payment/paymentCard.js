2518ced1f4db1d0d51991a14dca3daf416f7a80f
Gessica <santos.gessicacarolina@gmail.com>
2023-08-03 16:36:53 -0300

    feat: add function to check if user has a payment

--- src/components/Payment/paymentCard.js
+++ src/components/Payment/paymentCard.js
@@ -1,69 +1,26 @@
-import { useState } from 'react';
 import styled from 'styled-components';
-import Cards from 'react-credit-cards';
 import 'react-credit-cards/es/styles-compiled.css';
-import usePostPayment from '../../hooks/api/usePostPayment';
-import { toast } from 'react-toastify';
-import { getCreditCardNameByNumber } from 'creditcard.js';
+import PaymentForm from './paymentForm';
+import useGetPayment from '../../hooks/api/useGetPayment';
+import { useEffect, useState } from 'react';
 
 export default function CardForm({ tickets }) {
-  const { postPayment, postPaymentLoading } = usePostPayment();
+  const { getPayment } = useGetPayment();
+  const [payment, setPayment] = useState(null);
 
-  // eslint-disable-next-line no-console
-  console.log(tickets);
-
-  const [cardInfo, setCardInfo] = useState({
-    number: '',
-    name: '',
-    expiry: '',
-    cvc: '',
-    focused: '',
-  });
-
-  const enviaAqui = async(e) => {
-    e.preventDefault();
-    delete cardInfo.focused;
-
-    const issuer = getCreditCardNameByNumber(cardInfo.number);
-    // eslint-disable-next-line no-console
-    console.log(issuer);
-    if (issuer === 'Credit card is invalid!') {
-      return toast('Número de cartão inválido!');
-    }
-    const newData = {
-      ticketId: tickets.id,
-      cardData: {
-        issuer: issuer,
-        number: cardInfo.number,
-        name: cardInfo.name,
-        expirationDate: cardInfo.expiry,
-        cvv: cardInfo.cvc,
-      },
+  useEffect(() => {
+    const fetchPaymentData = async() => {
+      try {
+        const paymentData = await getPayment(tickets.id);
+        setPayment(paymentData);
+      } catch (err) {
+        // eslint-disable-next-line no-console
+        console.log(err);
+      }
     };
 
-    // eslint-disable-next-line no-console
-    console.log('newData', newData);
-
-    try {
-      await postPayment(newData);
-      toast('Pagamento feito com sucesso!');
-    } catch (err) {
-      // eslint-disable-next-line no-console
-      console.log(err);
-      toast('Não foi possível fazer o pagamento.');
-    }
-  };
-
-  const handleInputFocus = (e) => {
-    setCardInfo({ ...cardInfo, focused: e.target.name });
-  };
-
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    const maxLength = name === 'cvc' ? 3 : undefined;
-    const limitedValue = maxLength ? value.slice(0, maxLength) : value;
-    setCardInfo({ ...cardInfo, [name]: limitedValue });
-  };
+    fetchPaymentData();
+  }, [getPayment, tickets.id]);
 
   return (
     <PaymentContent>
@@ -79,111 +36,10 @@ export default function CardForm({ tickets }) {
         </span>
       </TicketType>
       <PaymentMessage>Pagamento</PaymentMessage>
-      <PaymentContainer>
-        <PaymentForm onSubmit={enviaAqui}>
-          <InputAndCardContainer>
-            <Cards
-              number={cardInfo.number}
-              name={cardInfo.name}
-              expiry={cardInfo.expiry}
-              cvc={cardInfo.cvc}
-              focused={cardInfo.focused}
-            />
-            <InputsContainer>
-              <input
-                name="number"
-                type="text"
-                maxLength={16}
-                placeholder="Card Number"
-                value={cardInfo.number}
-                onChange={handleInputChange}
-                onFocus={handleInputFocus}
-              />
-              <p>E.g.: 49...,51...,36...,37...</p>
-              <input
-                name="name"
-                placeHolder="Name"
-                type="text"
-                value={cardInfo.name}
-                onChange={handleInputChange}
-                onFocus={handleInputFocus}
-              />
-              <FormCVCAndExpiryDate>
-                <input
-                  name="expiry"
-                  placeHolder="Valid Thru"
-                  type="text"
-                  maxLength="4"
-                  mask="99/99"
-                  value={cardInfo.expiry}
-                  onChange={handleInputChange}
-                  onFocus={handleInputFocus}
-                />
-                <input
-                  name="cvc"
-                  placeHolder="CVC"
-                  type="text"
-                  value={cardInfo.cvc}
-                  onChange={handleInputChange}
-                  onFocus={handleInputFocus}
-                />
-              </FormCVCAndExpiryDate>
-            </InputsContainer>
-          </InputAndCardContainer>
-          <Button type="submit" disabled={postPaymentLoading}>
-            Finalizar pagamento
-          </Button>
-        </PaymentForm>
-      </PaymentContainer>
+      {payment ? 'Em breve' : <PaymentForm tickets={tickets} />}
     </PaymentContent>
   );
 }
-
-const PaymentContainer = styled.div`
-  display: flex;
-  max-width: 400px;
-  gap: 15px;
-  font-family: 'Roboto', sans-serif;
-  @media (max-width: 600px) {
-    flex-direction: column;
-  }
-`;
-
-const PaymentForm = styled.form`
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-  input {
-    max-width: 290px;
-    height: 45px;
-    padding: 18.5px 14px;
-    border: 1px solid #bbb;
-    border-radius: 2px;
-  }
-  p {
-    font-size: 12px;
-    color: #808080;
-  }
-`;
-
-const InputAndCardContainer = styled.div`
-  display: flex;
-  gap: 15px;
-`;
-
-const InputsContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-`;
-
-const FormCVCAndExpiryDate = styled.div`
-  display: flex;
-  gap: 10px;
-  input:last-child {
-    max-width: 80px;
-  }
-`;
 
 const PaymentContent = styled.div`
   display: flex;
@@ -223,21 +79,4 @@ const TicketType = styled.div`
     color: #898989;
     font-size: 14px;
   }
-`;
-
-const Button = styled.button`
-  max-width: 182px;
-  height: 37px;
-  border: 0;
-  border-radius: 4px;
-  background: #e0e0e0;
-  box-shadow: 0px 2px 10px 0px rgba(0, 0, 0, 0.25);
-
-  color: #000;
-  text-align: center;
-  font-family: 'Roboto', sans-serif;
-  font-size: 14px;
-  font-style: normal;
-  font-weight: 400;
-  line-height: normal;
 `;
--- src/services/paymentApi.js
+++ src/services/paymentApi.js
@@ -9,3 +9,13 @@ export async function postPayment(body, token) {
 
   return response.data;
 }
+
+export async function getPayment(ticketId, token) {
+  const response = await api.get(`/payments?ticketId=${ticketId}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  return response.data;
+}
